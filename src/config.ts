fa65b5935ff8faa444a7923b3efbade8c6059ebf
ReekyStive <reekystive@gmail.com>
2024-08-27 22:39:07 +0800

    feat: ✨ add lowdb support

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,10 @@
-import { readFile } from 'fs/promises';
+import { existsSync } from 'fs';
+import { Low } from 'lowdb';
 import { objectToCamel } from 'ts-case-convert';
+import type { ObjectToCamel } from 'ts-case-convert/lib/caseConvert.js';
 import YAML from 'yaml';
+
+import { DataFile } from 'lowdb/node';
 
 interface RawChallenge {
   correct_answer: string;
@@ -12,9 +16,25 @@ interface RawChallenges {
   challenges: RawChallenge[];
 }
 
-const configDir = new URL('../config/', import.meta.url);
-const challengesYaml = await readFile(new URL('challenges.yaml', configDir), 'utf-8');
-const rawChallenges = (YAML.parse(challengesYaml) as RawChallenges).challenges;
+export type Challenge = ObjectToCamel<RawChallenge>;
 
-export const challenges = objectToCamel(rawChallenges);
-export type Challenge = (typeof challenges)[number];
+const configDirUrl = new URL('../config/', import.meta.url);
+const challengeYamlUrl = new URL('challenges.yaml', configDirUrl);
+const challengeLocalYamlUrl = new URL('challenges.local.yaml', configDirUrl);
+
+let challengesYamlUrl: URL;
+if (process.env.NODE_ENV === 'development' && existsSync(challengeLocalYamlUrl)) {
+  challengesYamlUrl = challengeLocalYamlUrl;
+} else {
+  challengesYamlUrl = challengeYamlUrl;
+}
+
+const dataFile = new DataFile(challengesYamlUrl, {
+  parse: YAML.parse,
+  stringify: YAML.stringify,
+});
+
+export const yamlChallengeDb = new Low<RawChallenges>(dataFile, { challenges: [] });
+await yamlChallengeDb.read();
+
+export const challenges = objectToCamel(yamlChallengeDb.data).challenges;
